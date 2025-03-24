1147bd3785d7bc653d9742d675f7218bb4df3ab7
hhsNel <hihijsonel@github.com>
2025-03-24 09:04:58 +0100

    basic aliases

---
+++ aliases.sh
@@ -0,0 +1,4 @@
+alias getcg="ad -c 1 -N -e nekos -d -S -h -R -V -O -x 0 -y -1 -b"
+alias getncg="ad -c 1 -n -e nekos -d -S -h -R -V -O -x 0 -y -1 -b"
+alias getcgs="ad -c 5 -N -e nekos -d -s 5000000 -h -r -V -O -x 0 -y 0 -b"
+alias getncgs="ad -c 5 -n -e nekos -d -s 5000000 -h -r -V -O -x 0 -y 0 -b"
